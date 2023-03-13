75657618fe494f9c17604c9915e0e2c7a158fd26
akshat652 <114381239+akshat652@users.noreply.github.com>
2023-03-13 19:33:33 +0530

    Add files via upload

---
+++ To delete rows from 2D array.py
@@ -0,0 +1,9 @@
+array=[[23,45,43,23,45],[45,67,54,32,45],[89,90,87,65,44],[23,45,67,32,10]]
+
+#Delete row values of 3rd row
+del array[2]
+
+#Delete row values of 2nd row
+del array[1]
+
+print(array)
